567950f7de678a43e56e10bb92c28dfe47029bb9
ntlk <hi@ntlk.net>
2016-04-14 17:34:49 +0100

    Record which place the caller will be talking about

--- respond.rb
+++ respond.rb
@@ -8,6 +8,16 @@ get '/' do
   content_type 'text/xml'
 
   Twilio::TwiML::Response.new do |r|
-    r.Say 'Hello, this is an example of a program which uses Twilio.'
+    r.Say 'Hello, you are using the service which lets you send audio postcards.'
+    r.Say 'Today’s theme is Places. Think of a story you can tell someone about a specific place.'
+    r.Record maxlength: 30, action: '/place', method: 'get', transcribe: true, transcribeCallback: '/place-transcription', playBeep: false
   end.text
+end
+
+get '/place' do
+  content_type 'text/xml'
+end
+
+get 'place-transcription' do
+  content_type 'text/xml'
 end
