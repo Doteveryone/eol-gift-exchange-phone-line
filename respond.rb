56905f165d1e50d0e22715149db0ce5f4c2f5a0d
ntlk <hi@ntlk.net>
2016-04-14 17:46:53 +0100

    Show the recording URL in the longs and output the transcript

--- respond.rb
+++ respond.rb
@@ -8,7 +8,7 @@ get '/' do
   content_type 'text/xml'
 
   Twilio::TwiML::Response.new do |r|
-    r.Say 'Hello, you are using the service which lets you send audio postcards.'
+    r.Say 'Hello, you are using a service which lets you send audio postcards.'
     r.Say 'Today’s theme is Places. Think of a story you can tell someone about a specific place.'
     r.Record maxlength: 30, action: '/place', method: 'get', transcribe: true, transcribeCallback: '/place-transcription', playBeep: false
   end.text
@@ -16,8 +16,13 @@ end
 
 get '/place' do
   content_type 'text/xml'
+  puts params['RecordingUrl']
+  Twilio::TwiML::Response.new do |r|
+    r.Say params['RecordingUrl']
+  end.text
 end
 
-get 'place-transcription' do
+post 'place-transcription' do
   content_type 'text/xml'
+  puts params['TranscriptionText']
 end
