fcfbc5f3c89763c067db08bbf2bada5cbeb00c54
ntlk <hi@ntlk.net>
2016-04-15 14:22:43 +0100

    Create a new story when a call is initiated

--- respond.rb
+++ respond.rb
@@ -8,8 +8,13 @@ Dotenv.load
 set :logging, true
 set :database, { adapter: 'postgresql' }
 
+class Story < ActiveRecord::Base
+end
+
 get '/' do
   content_type 'text/xml'
+
+  Story.create(call_sid: params['CallSid'])
 
   Twilio::TwiML::Response.new do |r|
     r.Say 'Hello, you are using a service which lets you send audio postcards.'
@@ -21,7 +26,7 @@ end
 
 get '/place' do
   content_type 'text/xml'
-  puts params['RecordingUrl']
+  recording_url = params['RecordingUrl']
   Twilio::TwiML::Response.new do |r|
     r.Say 'Thank you. Now tell me your story. When finished, stop speaking or press star.'
     r.Record maxlength: 3600, action: '/story', method: 'get', playBeep: false, finishOnKey: '*'
@@ -30,7 +35,7 @@ end
 
 get '/story' do
   content_type 'text/xml'
-  puts params['RecordingUrl']
+  recording_url = params['RecordingUrl']
   Twilio::TwiML::Response.new do |r|
     r.Say 'Thank you. Now please tell me the full name of the recipient.'
     r.Record maxlength: 20, action: '/recipient', method: 'get', transcribe: true, transcribeCallback: '/recipient-transcription', playBeep: false
@@ -39,7 +44,7 @@ end
 
 get '/recipient' do
   content_type 'text/xml'
-  puts params['RecordingUrl']
+  recording_url = params['RecordingUrl']
   Twilio::TwiML::Response.new do |r|
     r.Say 'Thank you. I will send the story postcard on your behalf.'
     r.Hangup
