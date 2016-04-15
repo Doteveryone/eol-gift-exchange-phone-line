cb3ec28b43dfc0240b22a6736d5e3b63fb7906f3
ntlk <hi@ntlk.net>
2016-04-15 14:34:29 +0100

    Make sure we can set up the db on Heroku

---
+++ db/migrate/20160415130510_create_stories.rb
@@ -0,0 +1,13 @@
+class CreateStories < ActiveRecord::Migration
+  def change
+    create_table :stories do |t|
+      t.string :sender
+      t.string :sender_audio
+      t.string :recipient
+      t.string :recipient_audio
+      t.string :story_audio
+      t.string :call_sid
+      t.boolean :sent
+    end
+  end
+end
