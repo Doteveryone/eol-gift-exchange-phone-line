e10268acdfa72aa6d3ae7b5c970c55d507517232
ntlk <hi@ntlk.net>
2016-04-15 16:32:59 +0100

    Record the sender’s name

--- respond.rb
+++ respond.rb
@@ -18,7 +18,21 @@ get '/' do
   Twilio::TwiML::Response.new do |r|
     r.Say 'Hello, you are using a service which lets you send audio postcards.'
     r.Say 'Today’s theme is Places. Think of a story you can tell someone about a specific place.'
-    r.Say 'I will ask you to record the story, but first please say the name of the place now.'
+    r.Say 'I will ask you to record the story, but first please say your name.'
+    r.Record maxlength: 15, action: '/sender', method: 'get', transcribe: true, transcribeCallback: '/sender-transcription', playBeep: false
+  end.text
+end
+
+get '/sender' do
+  content_type 'text/xml'
+
+  call_sid = params['CallSid']
+  story = Story.find_by_call_sid(call_sid)
+  sender_audio = params['RecordingUrl']
+  story.update(sender_audio: sender_audio)
+
+  Twilio::TwiML::Response.new do |r|
+    r.Say 'Thank you. Now state the name of the place your story is about.'
     r.Record maxlength: 20, action: '/place', method: 'get', transcribe: true, transcribeCallback: '/place-transcription', playBeep: false
   end.text
 end
@@ -63,6 +77,11 @@ get '/recipient' do
     r.Say 'Thank you. I will send the story postcard on your behalf.'
     r.Hangup
   end.text
+end
+
+post '/sender-transcription' do
+  content_type 'text/xml'
+  puts params['TranscriptionText']
 end
 
 post '/place-transcription' do
