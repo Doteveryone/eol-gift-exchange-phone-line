a76de71c4ccac5a1bf6511dc1065a0c0ea753027
ntlk <hi@ntlk.net>
2016-04-19 16:22:49 +0100

    Make the thinking thing work

--- respond.rb
+++ respond.rb
@@ -23,7 +23,7 @@ get '/' do
     r.Say 'Today’s theme is Places. Think of a story you can tell someone about a specific place.'
     r.Say 'I will ask you a few questions to fill out the postcard. I will ask your name, the recipient’s name, the name of the place you can tell a story about and finally, the story itself.'
     r.Say 'I will play some muzak while you have a think about what to say. When you’re ready press any key and I will begin asking the questions.'
-    r.Enqueue waitUrl: '/thinking', waitUrlMethod: 'get'
+    r.Redirect '/thinking', method: 'get'
   end.text
 end
 
@@ -31,7 +31,7 @@ get '/thinking' do
   content_type 'text/xml'
 
   Twilio::TwiML::Response.new do |r|
-    r.Gather numdigits: '1', action: '/record', method: 'get' do |g|
+    r.Gather numdigits: '1', action: '/record', method: 'get', timeout: 3600 do |g|
       g.Play 'http://prototyping-temp-files.s3.amazonaws.com/Malaventura_-_13_-_Convicted_Jazz_Drones.mp3'
     end
   end.text
